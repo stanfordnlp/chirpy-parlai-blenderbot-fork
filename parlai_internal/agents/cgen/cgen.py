9ef0bddf5d4e64bf167869c18b011320aff97d2a
Trenton Chang (tchang97-eval) <tchang97@stanford.edu>
2021-04-28 05:35:45 +0000

    add prefix mode flag to custom agent _generate method

--- parlai_internal/agents/cgen/cgen.py
+++ parlai_internal/agents/cgen/cgen.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python3
 import logging
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 
 import torch
+import torch.nn.functional as F
 
 from parlai.agents.transformer.transformer import TransformerGeneratorAgent
 from parlai.core.metrics import (
     GlobalAverageMetric,
     GlobalTimerMetric,
 )
-from parlai.core.torch_agent import Output
+from parlai.core.torch_agent import Output, Batch
 from parlai.core.message import Message
 from parlai.utils.distributed import is_primary_worker
 from parlai.utils.misc import warn_once
+from parlai.utils.torch import neginf
 
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARN)
 
 class CgenAgent(TransformerGeneratorAgent): # CGen = Conditional GENeration -- i.e. based on prefix. Sorry, PARLAI parsing means I have to make a jank name.
+    
+    def __init__(self, *args, **kwargs):
+        logger.info("Initializing a Conditional GENeration (CGEN) Agent (like transformer/generator).")
+        super().__init__(*args, **kwargs)
 
     def act(self, prefix_text=None):
         """
@@ -27,6 +36,7 @@ class CgenAgent(TransformerGeneratorAgent): # CGen = Conditional GENeration -- i
             prefix_tensor = torch.LongTensor(self.history.parse(prefix_text)).unsqueeze(0)
         else:
             prefix_tensor = None
+        logger.info("Prefix tensor is tokenized as " + str(prefix_tensor))
         response = self.batch_act([self.observation], prefix_tensor)[0]
         return response
 
@@ -181,3 +191,129 @@ class CgenAgent(TransformerGeneratorAgent): # CGen = Conditional GENeration -- i
         if not self.skip_generation:
             retval.beam_texts = beam_texts
         return retval
+
+    
+    def _generate(
+            self,
+            batch: Batch,
+            beam_size: int,
+            max_ts: int,
+            prefix_tokens: Optional[torch.LongTensor] = None,
+        ):
+            """
+            Generate an output with beam search.
+
+            Depending on the options, this may perform greedy/topk/nucleus generation.
+
+            :param Batch batch:
+                Batch structure with input and labels
+            :param int beam_size:
+                Size of each beam during the search
+            :param int max_ts:
+                the maximum length of the decoded sequence
+            :param prefix_tokens:
+                if given, a tensor of tokens that must begin the decoded sequence.
+
+            :return:
+                tuple (beam_pred_scores, beams)
+
+                - beam_preds_scores: list of (prediction, score) pairs for each sample in
+                  Batch
+                - beams :list of Beam instances defined in Beam class, can be used for any
+                  following postprocessing, e.g. dot logging.
+            """
+            model = self.model
+            if isinstance(model, torch.nn.parallel.DistributedDataParallel):
+                model = self.model.module
+            encoder_states = model.encoder(*self._encoder_input(batch))
+            if batch.text_vec is not None:
+                dev = batch.text_vec.device
+            else:
+                assert batch.label_vec is not None, "need label_vec for _generate"
+                dev = batch.label_vec.device
+
+            bsz = batch.batchsize
+            if batch.text_vec is not None:
+                batchsize = batch.batchsize
+                beams = [
+                    self._treesearch_factory(dev)
+                    .set_context(self._get_context(batch, batch_idx))
+                    .set_block_list(self.beam_block_list)
+                    for batch_idx in range(batchsize)
+                ]
+            else:
+                beams = [self._treesearch_factory(dev) for _ in range(bsz)]
+
+            # repeat encoder outputs and decoder inputs
+            decoder_input = self._get_initial_decoder_input(bsz, beam_size, dev)
+
+            inds = torch.arange(bsz).to(dev).unsqueeze(1).repeat(1, beam_size).view(-1)
+            encoder_states = model.reorder_encoder_states(encoder_states, inds)
+            incr_state = None
+
+            for _ts in range(max_ts):
+                if all((b.is_done() for b in beams)):
+                    # exit early if possible
+                    logger.debug(f"Decoding exits on step #{_ts}")
+                    break
+                logger.debug(f"Decoding step #{_ts}")
+
+                score, incr_state = model.decoder(decoder_input, encoder_states, incr_state)
+                # only need the final hidden state to make the word prediction
+                score = score[:, -1:, :]
+                score = model.output(score)
+                # score contains softmax scores for bsz * beam_size samples
+                score = score.view(bsz, beam_size, -1)
+                if self.temperature != 1.0:
+                    score.div_(self.temperature)
+                # force to fp32 to avoid overflow issues during search calculations
+                score = F.log_softmax(score, dim=-1, dtype=torch.float32)  # type: ignore
+
+                in_prefix_mode = (prefix_tokens is not None and _ts < prefix_tokens.size(1))
+                if in_prefix_mode:
+                    # generate prefix_tokens for every timestep that they exist
+                    # achieve by setting score of all other tokens to be -inf
+                    prefix_toks = prefix_tokens[:, _ts].unsqueeze(-1).repeat(1, beam_size)
+                    prefix_score = score.gather(-1, prefix_toks.unsqueeze(-1))
+                    prefix_mask = prefix_toks.ne(self.NULL_IDX)
+                    score[prefix_mask] = neginf(score.dtype)
+                    score[prefix_mask] = score[prefix_mask].scatter_(
+                        -1,
+                        prefix_toks[prefix_mask].unsqueeze(-1),
+                        prefix_score[prefix_mask],
+                    )
+
+                for i, b in enumerate(beams):
+                    if not b.is_done():
+                        b.advance(score[i], in_prefix_mode)
+                incr_state_inds = torch.cat(
+                    [
+                        beam_size * i + b.get_backtrack_from_current_step()
+                        for i, b in enumerate(beams)
+                    ]
+                )
+                incr_state = model.reorder_decoder_incremental_state(
+                    incr_state, incr_state_inds
+                )
+                selection = torch.cat(
+                    [b.get_output_from_current_step() for b in beams]
+                ).unsqueeze(-1)
+                decoder_input = self._get_next_decoder_input(
+                    decoder_input, selection, incr_state_inds
+                )
+
+            # get all finalized candidates for each sample (and validate them)
+            n_best_beam_preds_scores = [b.get_rescored_finished() for b in beams]
+
+            if hasattr(self, '_rerank_beams'):
+                n_best_beam_preds_scores = self._rerank_beams(  # type: ignore
+                    batch, n_best_beam_preds_scores
+                )
+
+            # get the top prediction for each beam (i.e. minibatch sample)
+            beam_preds_scores = [n_best_list[0] for n_best_list in n_best_beam_preds_scores]
+
+            return beam_preds_scores, beams
+        
+    def max_check(self, var, step, message):
+        logger.debug(message + f", decoding step #{step}")
